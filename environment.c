fb30a39a43b300c901c828775f4efc1debc5df4f
jacon69 <jcondea@gmail.com>
2024-05-30 15:49:26 +0200

    Primera subida. bulitins en desarrollo

---
+++ environment.c
@@ -0,0 +1,186 @@
+#include "environment.h"
+#include "./Libft/libft.h"
+
+
+
+char	**ft_get_env(t_list  **env)
+{
+	t_list *p_env;
+	char **var_entorno;
+	int i;
+
+	p_env = *env;
+	i = 0;
+	while (p_env)
+	{
+		i++;
+		p_env = p_env->next;
+	}
+	var_entorno = (char **)malloc(sizeof(char *) * (i + 1)); //Hago Malloc
+	if (!var_entorno)
+	{
+		perror("Error en malloc");
+		ft_free_list(env);
+		return NULL;
+	}
+	i = 0;
+	p_env = *env;
+	while (p_env)
+	{
+		var_entorno[i] = ft_strdup(p_env->content); //Malloc
+		if (!var_entorno[i]) //Protejo malloc
+		{
+			ft_free_char(var_entorno);
+			ft_free_list(env);
+			perror("Error en malloc");
+			return NULL;
+		}
+		i++;
+		p_env = p_env->next;
+	}
+	var_entorno[i] = NULL;
+	return (var_entorno);
+}
+
+
+t_list **ft_ini_env(char **environment)
+{
+    int i;
+
+    t_list **env;
+    t_list  *p_env;
+
+    env = (t_list **)malloc(sizeof(t_list *));  //Hago Malloc
+    if (!env) //Protejo malloc
+	{
+		perror("Error en malloc");
+        return NULL;
+	}
+	i = 0;
+	p_env = ft_lstnew(ft_strdup(environment[i])); //Hago Malloc
+	if (!p_env)  //Protejo malloc
+	{
+		free(env);
+		perror("Error en malloc");
+		return NULL;
+	}
+	ft_lstadd_back(env, p_env);
+    while (environment[++i])
+    {
+        p_env = ft_lstnew(ft_strdup(environment[i])); //Hago Malloc
+        ft_lstadd_back(env, p_env);
+		if (!p_env)  //Protejo malloc
+		{
+			ft_free_list(env); //Libero la memoria de la lista de variables de entorno y dejo env apuntando NULL
+			return NULL;
+		}
+	}
+ 	return env;
+}
+
+void ft_del_v_env(char *var_env, t_list **env)//Borro si existe variable de entorno.
+{
+	t_list *p_env;
+	t_list *aux;
+	t_list *aux2; //previo
+	char *str_aux;
+	size_t nlong;
+
+	str_aux = ft_strdup((const char *)var_env); //Hago Malloc
+	if (!str_aux) //Protejo malloc
+	{
+		ft_free_list(env);
+		perror("Error en malloc");
+		return ;
+	}
+
+	printf("str_aux: %s\n", str_aux);
+	p_env = *env;
+	aux2 = NULL;
+	while (p_env)
+	{
+		nlong  = ft_pos_chr(p_env->content, '='); //Busco la posición del igual
+		if ((ft_strncmp(p_env->content, str_aux, ft_strlen(str_aux)) == 0) && (nlong == ft_strlen(str_aux))) //Si la variable de entorno existe la borro
+		{
+			aux = p_env->next;
+			free(p_env->content);
+			free(p_env);
+			p_env = aux;
+			if (aux2)
+				aux2->next = p_env;
+			else
+				*env = p_env;
+		}
+		else
+		{
+			aux2 = p_env;
+			p_env = p_env->next;
+		}
+	}
+		free(str_aux);
+}
+
+void ft_add_v_env(char *var_env, t_list **env)//Añado variable de entorno. Lo hago con export
+{
+	t_list *p_env;
+	char *aux;
+	char *var;
+
+	var = ft_strndup((const char *)var_env, (size_t)ft_pos_chr(var_env, '=')); //Hago Malloc
+	if (!var) //Protejo malloc
+	{
+		ft_free_list(env);
+		perror("Error en malloc");
+		return ;
+	}
+	p_env = *env;
+	while (p_env)
+	{
+		if (ft_strncmp(p_env->content, var, ft_strlen(var)) == 0) //Recorro la lista para ver si ya existe la variable de entorno
+		{
+			aux= p_env->content;
+			p_env->content = ft_strdup(var_env); //Hago Malloc
+			if (!p_env->content) //Protejo malloc
+			{
+				free(var);
+				ft_free_list(env);
+				perror("Error en malloc");
+				return ;
+			}
+			free(aux);
+			free(var);
+			return ;
+		}
+		p_env = p_env->next;
+	}
+	p_env = ft_lstnew(ft_strdup(var_env)); //Hago Malloc
+	if (!p_env) //Protejo malloc
+	{
+		ft_free_list(env); //Libero la memoria de la lista de variables de entorno y dejo env apuntando NULL
+		free(var);
+		perror("Error en malloc");
+		return ;
+	}
+	free(var);
+	ft_lstadd_back(env, p_env); //Aquí añado el nodo nuevo a la lista
+}
+
+void	ft_pwd(t_list **env)
+{
+	t_list	*p_env;
+	char 	*var;
+
+	p_env = *env;
+	while (p_env)
+	{
+		if (ft_strncmp(p_env->content, "PWD", 3) == 0) //Recorro la lista para ver si ya existe la variable de entorno
+		{
+			var = p_env->content;
+			var = var + 4;
+			ft_printf("%s\n", var);
+			
+			return ;
+		}
+	p_env = p_env->next;
+	}
+}
---
+++ prom.c
@@ -0,0 +1,86 @@
+#include "environment.h"
+
+
+void ft_ejecutar(char *line, t_list  **env)
+{
+    char **args;
+
+    args = ft_split(line, ' ');
+    if (!args)
+        return;
+    if (ft_strncmp(args[0], "env",3) == 0)
+    {
+        ft_lstiter(*env, print_string); //##PRUEBA## Imprime la lista de variables de entorno
+    }
+    else if ((ft_strncmp(args[0], "export", 6) == 0) && ((ft_memchr(args[1], '=', ft_strlen(args[1]))!= NULL)))
+    {
+        ft_add_v_env(args[1], env);
+    }
+	else if ((ft_strncmp(args[0], "export", 6) == 0) && (args[1] == NULL))
+	{
+		ft_lstiter(*env, print_string); //##PRUEBA## Imprime la lista de variables de entorno
+	}
+	else if (ft_strncmp(args[0], "export", 6) == 0)
+		return;
+    else if (ft_strncmp(args[0], "unset", 5) == 0)
+    {
+        ft_del_v_env(args[1], env); 
+    }
+    else if (ft_strncmp(args[0], "cd", 2) == 0)
+    {
+      //  ft_chdir(args[1]);                   //Crear un ft_chdir
+    }
+    else if (ft_strncmp(args[0], "pwd", 3) == 0)
+	{
+		ft_pwd(env);                         //Crear un ft_pwd
+	}
+    else if (ft_strncmp(args[0], "echo", 4) == 0)
+        	ft_printf("%s\n", args[1]);
+    else if (ft_strncmp(args[0], "exit", 4) == 0)
+    {
+        ft_free_char(args);
+        ft_free_list(env);
+        exit(0);
+    }
+    else
+        printf("Comando no encontrado\n");
+}
+
+void prom(t_list  **env) 
+{
+    char *line;
+    int fd = open("prom.txt", O_WRONLY | O_CREAT | O_APPEND, 0644);
+    if (fd == -1) {
+        perror("No se pudo abrir el archivo");
+        return;
+    }
+
+    while (1) {
+        // Mostrar el prompt y leer una línea de entrada
+        line = readline("prom> "); //hace Malloc
+        if (!line)
+            break; // EOF, probablemente Ctrl+D Ctrl+c
+        if (ft_strlen(line) > 0) // Añadir la línea al historial
+            add_history(line);
+        // Escribir la línea en el archivo y añadir un salto de línea
+        if (write(fd, line, strlen(line)) == -1 || write(fd, "\n", 1) == -1)    
+        {
+            perror("Error al escribir en el archivo");
+            free(line);
+            break;
+        }
+         
+        if (ft_strncmp(line, "exit", 4) == 0) // Si la línea es "exit", salir del bucle
+        {
+            free(line);
+            break;
+        }
+        ft_ejecutar(line, env); //PAra pruebas en esta función pongo los comando que quiero probar
+
+        // Aquí es donde de se tiene que procesar la linea introducida
+        free(line); // Liberar la memoria de la línea
+    }
+
+    
+    close(fd);
+}
