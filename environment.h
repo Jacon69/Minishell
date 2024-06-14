ad46e3934abc5bce7b2e751856498876d9380f9c
jacon69 <jcondea@gmail.com>
2024-06-14 10:31:03 +0200

    Avance en los build-in

--- environment.h
+++ environment.h
@@ -9,7 +9,38 @@
 # include <fcntl.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+#include <dirent.h>	
 # include "./Libft/libft.h"
+//De la estructura de alexi
+
+typedef struct s_command
+{
+	int		index;
+	char	*command;
+	char	path[1024];
+	char	**args;
+	int		redir1; //0= NULL, < = 1, << = 2
+	int		redir2; //0= NULL, > = 1, >> = 2
+	int		piped; //0 no hay pipe, 1 hay pipe (salida)
+	int		file_input;
+	int		file_output;
+	char	*input; //Entrada desde archivos o pipes
+	char	*string_output;
+	int		returned_output;
+}	t_command;
+
+typedef struct s_myshell
+{
+	t_list 		**env;
+	char		*actual_dir;
+	int			nbr_command;
+	int			pipe;
+	int			last_status;
+	t_command	*commands;
+}	t_myshell;
+
+
+ft_build_int(t_myshell shell);
 
 void prom(t_list  **env) ;
 
